b3bde6d30fb55d3a7360dadf25edf7008344e456
AsterixBr <xandsbarbosa@gmail.com>
2021-06-08 11:48:55 -0300

    udpate jss

---
+++ jscript/objeto.js
@@ -0,0 +1,9 @@
+var hotel = {
+    nome: 'Quay',
+    quartos: 40,
+    reservados: 25,
+    academia: true,
+    cehcaDisponibilidade: function() {
+        return this.quartos - this.reservados;
+    }
+}
