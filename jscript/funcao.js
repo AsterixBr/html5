6d5e9d1f16519b5a3a7686188d7b7df1be8a7085
AsterixBr <xandsbarbosa@gmail.com>
2021-06-08 10:45:52 -0300

    udpate

--- jscript/funcao.js
+++ jscript/funcao.js
@@ -33,3 +33,20 @@ function calculaAreaVolume(altura, largura, profundidade) {
     var resultadoAreaVolume = [area, volume];
     return resultadoAreaVolume;
 }
+
+//Utilizando a função e armazenando a posição desejada do array
+var calculoCaixaArea = calculaAreaVolume(10, 20, 30)[0];
+var calculoCaixaVolume = calculaAreaVolume(10, 20, 30)[1];
+
+//Exibindo os resultados
+document.write('<br> Área da Caixa é: ' + calculoCaixaArea);
+document.write('<br> Volume da Caixa é: ' + calculaAreaVolume);
+
+/*Podemos chamar a função diretamente na declaração da variável. Isso é uma função anônima.*/
+var areaCaixa = (function() {
+    var calculoarea = 10 * 20;
+    return calculoarea;
+}());
+
+//Exibindo o Resultado
+document.write('<br> A Função Anônima faz a mesma coisa: ' + areaCaixa);
